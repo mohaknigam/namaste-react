203fcf6ad4d1dd18e6dc6b519b39873145f47297
unknown <mohaknigam20@gmail.com>
2023-07-08 20:42:49 +0530

    lecture2

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "Namaste React";
-document.getElementById("root").appendChild(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Namaste React";
+// document.getElementById("root").appendChild(heading);
+
+const heading = React.createElement("h1", {}, "Namaste React 2!!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
